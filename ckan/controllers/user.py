ff84ecec3ed6c2353a8772d87562dbcd4efdf12b
Wen Li <spacelis@gmail.com>
2015-11-16 14:57:04 +0000

    Allowing partial search in password recovery may result in problems of accidental matches. It would be better to let users recover their passwords by entering their email addresses.

--- ckan/controllers/user.py
+++ ckan/controllers/user.py
@@ -441,29 +441,7 @@ class UserController(base.BaseController):
                 user_dict = get_action('user_show')(context, data_dict)
                 user_obj = context['user_obj']
             except NotFound:
-                # Try searching the user
-                try:
-                    del data_dict['id']
-                    data_dict['q'] = id
-
-                    if id and len(id) > 2:
-                        user_list = get_action('user_list')(context, data_dict)
-                        if len(user_list) == 1:
-                            # This is ugly, but we need the user object for the
-                            # mailer,
-                            # and user_list does not return them
-                            del data_dict['q']
-                            data_dict['id'] = user_list[0]['id']
-                            user_dict = get_action('user_show')(context, data_dict)
-                            user_obj = context['user_obj']
-                        elif len(user_list) > 1:
-                            h.flash_error(_('"%s" matched several users') % (id))
-                        else:
-                            raise NotFound
-                    else:
-                        raise NotFound
-                except:
-                    h.flash_error(_('No such user: %s') % id)
+                h.flash_error(_('No such user: %s') % id)
 
             if user_obj:
                 try:
